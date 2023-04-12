3ecfb516e01b70e075a15ca66913d1786c34b3e0
konyip <130008127+konyip@users.noreply.github.com>
2023-04-12 15:18:55 -0700

    poke

--- controller/build/ble5-ctr/main.c
+++ controller/build/ble5-ctr/main.c
@@ -20,7 +20,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  * 
- * BLAH BLAH BLAH POKE POKE POKE POKE POKE POKE
+ * BLAH BLAH BLAH POKE POKE POKE POKE POKE POKE POKE
  */
 /*************************************************************************************************/
 
